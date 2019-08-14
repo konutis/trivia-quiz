3dbe17d84ed4ed9d78473eaa0019e54a3fab3028
Janis K <janis.k@visualcomposer.com>
2019-08-14 22:17:59 +0300

    Add base files

---
+++ js.js
@@ -0,0 +1,237 @@
+(function() {
+  let startButton = document.querySelector('#start');
+  let startButton2 = document.querySelector('#start2');
+  let startButton3 = document.querySelector('#start3');
+  let howToButton = document.querySelector('#how-to');
+  let closeHowToButton = document.querySelector('#close-how-to');
+  let howToPlayScreen = document.querySelector('.how-to-play-screen');
+  let mainScreen = document.querySelector('.main-screen');
+  let quizScreen = document.querySelector('.quiz-screen');
+  let resultScreen = document.querySelector('.result-screen');
+
+  startButton.addEventListener('click', showGamePlay);
+  startButton2.addEventListener('click', showGamePlay);
+  startButton3.addEventListener('click', showGamePlay);
+
+  howToButton.addEventListener('click', function() {
+    howToPlayScreen.classList.remove('hidden');
+    mainScreen.classList.add('hidden');
+  });
+
+  closeHowToButton.addEventListener('click', function() {
+    howToPlayScreen.classList.add('hidden');
+    mainScreen.classList.remove('hidden');
+  });
+
+  let questionLength = 10;
+  let questionIndex = 0;
+  let score = 0;
+  let timer = null;
+  let questionData = null;
+  let usedQuestions = [];
+
+  function showGamePlay () {
+    questionIndex = 0;
+    score = 0;
+    mainScreen.classList.add('hidden');
+    howToPlayScreen.classList.add('hidden');
+    resultScreen.classList.add('hidden');
+    quizScreen.classList.remove('hidden');
+    let questionCount = document.getElementById('question-count');
+    questionCount.innerHTML = questionLength.toString();
+    populate();
+  }
+
+  let isEnded = function () {
+    return questionLength === questionIndex;
+  };
+
+  function populate() {
+    let answersContainer = document.getElementById('answers-container');
+    let answerButtons = answersContainer.querySelectorAll('.default-button');
+
+    answerButtons.forEach(function(element) {
+      element.disabled = false;
+      element.classList.remove('correct');
+      element.classList.remove('wrong');
+    });
+
+    if (isEnded()) {
+      showScores();
+    } else {
+      startProgressbar();
+      timer = window.setTimeout(function() {
+        guess(null);
+      }, 10000);
+
+      setQuizText('This cute wapuu is from');
+
+      questionData = getQuestionData();
+
+      let imageElement = document.getElementById('question-image');
+      imageElement.src = './assets/wapuus/' + questionData.image + '.png';
+
+      // show choices
+      let choices = questionData.answers;
+      for (let i = 0; i < choices.length; i++) {
+        let element = document.getElementById('answer' + i);
+        element.innerHTML = choices[i];
+
+        element.addEventListener('click', handleAnswerClick)
+      }
+
+      showProgress();
+    }
+  }
+
+  function getQuestionData () {
+    let randomIndex = getRandomIndexForQuestion();
+    let answers = getAnswers(data[randomIndex].city);
+    return {
+      image: data[randomIndex].image_name,
+      correct: data[randomIndex].city,
+      answers: answers
+    };
+  }
+
+  function getAnswers (correctAnswer) {
+    let answers = [correctAnswer];
+    for (let i = 0; i < 3; i++) {
+      let fakeAnswer = getFakeAnswer(answers);
+      answers.push(fakeAnswer);
+    }
+    return shuffle(answers);
+  }
+
+  function getFakeAnswer (answers) {
+    let randomIndex = getRandomIndex();
+    let fakeAnswer = data[randomIndex].city;
+    if (answers.indexOf(fakeAnswer) > -1) {
+      return getFakeAnswer(answers);
+    }
+    return fakeAnswer
+  }
+
+  function shuffle (array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+  function getRandomIndex () {
+    return Math.floor(Math.random() * data.length);
+  }
+
+  function getRandomIndexForQuestion () {
+    let index = Math.floor(Math.random() * data.length);
+    if (usedQuestions.indexOf(index) > -1) {
+      return getRandomIndex();
+    }
+    return index;
+  }
+
+  function handleAnswerClick (e) {
+    let el = e.currentTarget;
+    let answer = el.innerHTML;
+    el.removeEventListener('click', handleAnswerClick);
+    guess(answer);
+  }
+
+  function showProgress () {
+    let questionIndexElement = document.getElementById('question-index');
+    let index = questionIndex + 1;
+    questionIndexElement.innerHTML = index.toString();
+  }
+
+  function guess(answer) {
+    clearTimeout(timer);
+    let answersContainer = document.getElementById('answers-container');
+    let answerButtons = answersContainer.querySelectorAll('.default-button');
+
+    answerButtons.forEach(function(element) {
+      element.disabled = true;
+      if (element.innerHTML === questionData.correct) {
+        element.classList.add('correct');
+      }
+    });
+
+    stopProgressbar();
+
+    if (questionData.correct === answer) { // correct answer
+      score++;
+      setQuizText('WAPUUTASTIC … YOU’RE RIGHT!')
+    } else if (answer) { // incorrect answer
+      setQuizText('NICE TRY … YOU WERE CLOSE :P');
+      answerButtons.forEach(function(element) {
+        if (element.innerHTML === answer) {
+          element.classList.add('wrong');
+        }
+      });
+    } else {
+      setQuizText('YOUR TIME IS OUT … PO PO POOO')
+    }
+
+    questionIndex++;
+
+    window.setTimeout(function() {
+      populate();
+    }, 3000);
+  }
+
+  function setQuizText (text) {
+    let el = document.getElementById('quiz-text');
+    el.innerHTML = text;
+  }
+
+  function showScores () {
+    let scoreElement = document.getElementById('score');
+    let scoreTotalElement = document.getElementById('score-total');
+    let scoreNameElement = document.getElementById('score-name');
+    let scoreImage = document.getElementById('result-logo');
+    let twitterTweetButton = document.getElementById('twitter-tweet-button');
+
+    scoreElement.innerHTML = score.toString();
+    scoreTotalElement.innerHTML = questionLength.toString();
+
+    if (score < 4) {
+      scoreNameElement.innerHTML = 'Newbuu';
+      scoreImage.src = './assets/wapuu-dumb.png'
+      twitterTweetButton.href = 'https://twitter.com/intent/tweet?text=My score - Newbuu! ' + encodeURIComponent(location.href);
+    } else if (score < 7) {
+      scoreNameElement.innerHTML = 'Rookiepuu';
+      scoreImage.src = './assets/wapuu-rookie.png'
+      twitterTweetButton.href = 'https://twitter.com/intent/tweet?text=My score - Rookiepuu! ' + encodeURIComponent(location.href);
+    } else if (score < 10) {
+      scoreNameElement.innerHTML = 'Wapuu expertuu';
+      twitterTweetButton.href = 'https://twitter.com/intent/tweet?text=My score - Wapuu expertuu! ' + encodeURIComponent(location.href);
+      scoreImage.src = './assets/Game-Logo.png'
+    } else {
+      scoreNameElement.innerHTML = 'Grandmuuster';
+      scoreImage.src = './assets/wapuu-grandmaster.png'
+      twitterTweetButton.href = 'https://twitter.com/intent/tweet?text=My score - Grandmuuster! ' + encodeURIComponent(location.href);
+    }
+
+    quizScreen.classList.add('hidden');
+    resultScreen.classList.remove('hidden');
+  }
+
+  function startProgressbar() {
+    let progressbar = document.getElementById('progress-bar');
+    progressbar.innerHTML = '';
+    let progressbarInner = document.createElement('span');
+    progressbar.appendChild(progressbarInner);
+    progressbarInner.style.animationPlayState = 'running';
+  }
+
+  function stopProgressbar () {
+    let progressbar = document.getElementById('progress-bar');
+    let progressbarInner = progressbar.querySelector('span');
+    progressbarInner.style.animationPlayState = 'paused';
+  }
+})();
---
+++ wapuusData.js
@@ -0,0 +1,486 @@
+let data = [
+  {
+    "image_name": "001",
+    "city": "Manchester"
+  },
+  {
+    "image_name": "002",
+    "city": "Switzerland"
+  },
+  {
+    "image_name": "003",
+    "city": "Montreal"
+  },
+  {
+    "image_name": "004",
+    "city": "Montreal"
+  },
+  {
+    "image_name": "005",
+    "city": "Pennsylvania"
+  },
+  {
+    "image_name": "006",
+    "city": "Brighton"
+  },
+  {
+    "image_name": "007",
+    "city": "Montreal"
+  },
+  {
+    "image_name": "008",
+    "city": "Tokyo"
+  },
+  {
+    "image_name": "009",
+    "city": "Fukui"
+  },
+  {
+    "image_name": "010",
+    "city": "Tokyo"
+  },
+  {
+    "image_name": "011",
+    "city": "Kansai"
+  },
+  {
+    "image_name": "012",
+    "city": "Fukuoka"
+  },
+  {
+    "image_name": "013",
+    "city": "Vienna"
+  },
+  {
+    "image_name": "014",
+    "city": "Belgrade"
+  },
+  {
+    "image_name": "015",
+    "city": "Brisbane"
+  },
+  {
+    "image_name": "016",
+    "city": "Yamanashi"
+  },
+  {
+    "image_name": "017",
+    "city": "Philadelphia"
+  },
+  {
+    "image_name": "018",
+    "city": "Philadelphia"
+  },
+  {
+    "image_name": "019",
+    "city": "London"
+  },
+  {
+    "image_name": "020",
+    "city": "Tokyo"
+  },
+  {
+    "image_name": "021",
+    "city": "Edinburgh"
+  },
+  {
+    "image_name": "022",
+    "city": "Salt Lake City"
+  },
+  {
+    "image_name": "023",
+    "city": "Berlin"
+  },
+  {
+    "image_name": "024",
+    "city": "Sydney"
+  },
+  {
+    "image_name": "025",
+    "city": "Russia"
+  },
+  {
+    "image_name": "026",
+    "city": "Orlando"
+  },
+  {
+    "image_name": "027",
+    "city": "Orlando "
+  },
+  {
+    "image_name": "028",
+    "city": "New York"
+  },
+  {
+    "image_name": "029",
+    "city": "Toronto"
+  },
+  {
+    "image_name": "030",
+    "city": "Minneapolis"
+  },
+  {
+    "image_name": "031",
+    "city": "Hampton"
+  },
+  {
+    "image_name": "033",
+    "city": "Philadelphia"
+  },
+  {
+    "image_name": "034",
+    "city": "Sweden"
+  },
+  {
+    "image_name": "035",
+    "city": "Portland"
+  },
+  {
+    "image_name": "036",
+    "city": "London"
+  },
+  {
+    "image_name": "037",
+    "city": "Queensland"
+  },
+  {
+    "image_name": "038",
+    "city": "Cologne"
+  },
+  {
+    "image_name": "039",
+    "city": "Vienna"
+  },
+  {
+    "image_name": "041",
+    "city": "Ottawa"
+  },
+  {
+    "image_name": "042",
+    "city": "Ottawa"
+  },
+  {
+    "image_name": "044",
+    "city": "Vienna"
+  },
+  {
+    "image_name": "045",
+    "city": "Jacksonville"
+  },
+  {
+    "image_name": "046",
+    "city": "Belfast"
+  },
+  {
+    "image_name": "047",
+    "city": "Minneapolis"
+  },
+  {
+    "image_name": "048",
+    "city": "Orlando"
+  },
+  {
+    "image_name": "049",
+    "city": "Santander"
+  },
+  {
+    "image_name": "050",
+    "city": "Torino"
+  },
+  {
+    "image_name": "101",
+    "city": "Port Elizabeth"
+  },
+  {
+    "image_name": "102",
+    "city": "Atlanta"
+  },
+  {
+    "image_name": "103",
+    "city": "Lisbon"
+  },
+  {
+    "image_name": "104",
+    "city": "Osnabrück"
+  },
+  {
+    "image_name": "105",
+    "city": "San José"
+  },
+  {
+    "image_name": "106",
+    "city": "Jakarta"
+  },
+  {
+    "image_name": "107",
+    "city": "Bristol"
+  },
+  {
+    "image_name": "108",
+    "city": "Bangkok"
+  },
+  {
+    "image_name": "109",
+    "city": "Jakarta"
+  },
+  {
+    "image_name": "110",
+    "city": "Jakarta"
+  },
+  {
+    "image_name": "111",
+    "city": "Jakarta"
+  },
+  {
+    "image_name": "112",
+    "city": "Jakarta"
+  },
+  {
+    "image_name": "113",
+    "city": "Frankfurt"
+  },
+  {
+    "image_name": "114",
+    "city": "Shizuoka"
+  },
+  {
+    "image_name": "115",
+    "city": "Nashville"
+  },
+  {
+    "image_name": "116",
+    "city": "Orlando"
+  },
+  {
+    "image_name": "117",
+    "city": "Orlando"
+  },
+  {
+    "image_name": "118",
+    "city": "Santa Clarita"
+  },
+  {
+    "image_name": "119",
+    "city": "Helsinki"
+  },
+  {
+    "image_name": "120",
+    "city": "Brno"
+  },
+  {
+    "image_name": "121",
+    "city": "Skopje"
+  },
+  {
+    "image_name": "122",
+    "city": "Turku"
+  },
+  {
+    "image_name": "123",
+    "city": "Rochester"
+  },
+  {
+    "image_name": "124",
+    "city": "Tokyo"
+  },
+  {
+    "image_name": "125",
+    "city": "Lyon"
+  },
+  {
+    "image_name": "126",
+    "city": "Bucharest"
+  },
+  {
+    "image_name": "127",
+    "city": "Guadalajara"
+  },
+  {
+    "image_name": "128",
+    "city": "Denver"
+  },
+  {
+    "image_name": "129",
+    "city": "Fayetteville"
+  },
+  {
+    "image_name": "130",
+    "city": "Albuquerque"
+  },
+  {
+    "image_name": "131",
+    "city": "Osaka"
+  },
+  {
+    "image_name": "132",
+    "city": "Raleigh"
+  },
+  {
+    "image_name": "133",
+    "city": "Rome"
+  },
+  {
+    "image_name": "134",
+    "city": "Buffalo"
+  },
+  {
+    "image_name": "135",
+    "city": "Cardiff"
+  },
+  {
+    "image_name": "136",
+    "city": "Varna"
+  },
+  {
+    "image_name": "137",
+    "city": "Varna"
+  },
+  {
+    "image_name": "138",
+    "city": "Cologne"
+  },
+  {
+    "image_name": "139",
+    "city": "Cologne"
+  },
+  {
+    "image_name": "140",
+    "city": "Iloilo"
+  },
+  {
+    "image_name": "141",
+    "city": "Philadelphia"
+  },
+  {
+    "image_name": "142",
+    "city": "Manchester"
+  },
+  {
+    "image_name": "143",
+    "city": "Cambridge"
+  },
+  {
+    "image_name": "144",
+    "city": "Cambridge"
+  },
+  {
+    "image_name": "145",
+    "city": "Singapore"
+  },
+  {
+    "image_name": "146",
+    "city": "Tokyo"
+  },
+  {
+    "image_name": "147",
+    "city": "Dublin"
+  },
+  {
+    "image_name": "148",
+    "city": "Charleston"
+  },
+  {
+    "image_name": "150",
+    "city": "Paris"
+  },
+  {
+    "image_name": "151",
+    "city": "Ahmedabad"
+  },
+  {
+    "image_name": "152",
+    "city": "Ahmedabad"
+  },
+  {
+    "image_name": "153",
+    "city": "Dallas"
+  },
+  {
+    "image_name": "154",
+    "city": "Dallas"
+  },
+  {
+    "image_name": "155",
+    "city": "San José"
+  },
+  {
+    "image_name": "156",
+    "city": "Edinburgh"
+  },
+  {
+    "image_name": "157",
+    "city": "Jacksonville"
+  },
+  {
+    "image_name": "158",
+    "city": "Kochi"
+  },
+  {
+    "image_name": "159",
+    "city": "Mumbai"
+  },
+  {
+    "image_name": "160",
+    "city": "San José"
+  },
+  {
+    "image_name": "161",
+    "city": "Miami"
+  },
+  {
+    "image_name": "162",
+    "city": "Miami"
+  },
+  {
+    "image_name": "163",
+    "city": "Portland"
+  },
+  {
+    "image_name": "164",
+    "city": "Portland"
+  },
+  {
+    "image_name": "165",
+    "city": "Raleigh "
+  },
+  {
+    "image_name": "166",
+    "city": "Pune"
+  },
+  {
+    "image_name": "167",
+    "city": "Bordeaux"
+  },
+  {
+    "image_name": "168",
+    "city": "Larissa"
+  },
+  {
+    "image_name": "169",
+    "city": "Rochester"
+  },
+  {
+    "image_name": "170",
+    "city": "Osaka"
+  },
+  {
+    "image_name": "171",
+    "city": "Portland"
+  },
+  {
+    "image_name": "172",
+    "city": "Ottawa"
+  },
+  {
+    "image_name": "173",
+    "city": "Kobe"
+  },
+  {
+    "image_name": "175",
+    "city": "Nashville"
+  },
+  {
+    "image_name": "176",
+    "city": "Tokyo"
+  }
+];
